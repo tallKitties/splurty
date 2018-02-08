43abe142bbdefc7b3a119582071d87a8892c689f
Ben Kautt <bkautt73@gmail.com>
2018-02-08 09:16:57 -0800

    Added quotes. Set index to display random quote

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208030823) do
+ActiveRecord::Schema.define(version: 20180208054425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "quotes", force: :cascade do |t|
+    t.string   "saying"
+    t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
